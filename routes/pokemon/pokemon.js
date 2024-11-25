56bf843b2bf7ea59295dfb05c7b1c0387b698bed
Emil Nilsson <emil.nilsson@ductus.se>
2024-11-25 16:39:30 +0100

    Fix edgecases

--- routes/pokemon/pokemon.js
+++ routes/pokemon/pokemon.js
@@ -5,94 +5,99 @@ const mwCacheFlagger = require("../../middleware/cacheFlagger");
 const redis = require("../../init/redis");
 const spritesOnlyDefault = require("./filters");
 
-const getDataFrom3rdPartySource = async (pokemonNo) => {
-  console.time(
-    "Cache layer 1 Miss\nFetching from public datasource: pokeapi.co/api"
-  );
+/* 
+  Get and cache data from 3rd party datasource
+*/
+const apiRequest3rdPartyDataSource = async (pokemonNo) => {
+  console.time("Cache layer 1 Miss");
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonNo}`
   );
   const data = await response.json();
-  console.timeEnd(
-    "Cache layer 1 Miss\nFetching from public datasource: pokeapi.co/api"
-  );
+  console.timeEnd("Cache layer 1 Miss");
   return data;
+};
+
+const cacheRequest = async (key, data) => {
+  await redis.setex(key, 3600, JSON.stringify(data));
 };
 
 router.get(`/:pokemon`, mwCacheFlagger, async (req, res) => {
   const { useCache } = req;
   const pokemonNo = req.params.pokemon;
+  console.log("Requesting pokemonNo:", pokemonNo);
+  const cacheKeyLayer1 = pokemonNo;
 
-  const cacheDataLayer1 = useCache ? await redis.get(pokemonNo) : null;
+  const cacheDataLayer1 = await redis.get(cacheKeyLayer1);
+  // If not using cache or cache is empty
+  // Get from 3rd party datasource
   if (!cacheDataLayer1) {
-    // Get from 3rd party source
-    const dataFrom3rdParty = await getDataFrom3rdPartySource(pokemonNo);
-
-    // Set my redis cache
-    const cacheKeyLayer1 = pokemonNo;
-    await redis.setex(cacheKeyLayer1, 3600, JSON.stringify(dataFrom3rdParty));
-    return res.json(dataFrom3rdParty);
+    const data = await apiRequest3rdPartyDataSource(pokemonNo);
+    if (useCache) {
+      // Store detils in layer 1 cache
+      console.time("Cache pokeapi-data to layer 1");
+      await cacheRequest(cacheKeyLayer1, data);
+      console.timeEnd("Cache pokeapi-data to layer 1");
+    }
+    return res.json(data);
   }
 
+  // Get data from layer 1 cache
   console.time("Cache layer 1 Hit");
-  const resParsedFromCache = JSON.parse(cacheDataLayer1);
+  const cachedRes = JSON.parse(cacheDataLayer1);
   console.timeEnd("Cache layer 1 Hit");
 
-  return res.json(resParsedFromCache);
+  return res.json(cachedRes);
 });
 
 router.get(`/sprite/:pokemon`, mwCacheFlagger, async (req, res) => {
   const { useCache } = req;
   const pokemonNo = req.params.pokemon;
+  console.log("Requesting pokemonNo:", pokemonNo);
   const cacheKeyLayer2 = `${pokemonNo}:sprites`;
 
-  // call my lw endpoint
-  try {
-    const cachedDataLayer2 = useCache ? await redis.get(cacheKeyLayer2) : null;
+  const cacheDataLayer2 = await redis.get(cacheKeyLayer2);
+  // If not using cache or cache is empty
+  // Get from 3rd party datasource
+  if (!cacheDataLayer2) {
+    console.time("Cache layer 2 Miss");
+    // Check layer 1 of cache
+    const cacheKeyLayer1 = pokemonNo;
+    const cacheDataLayer1 = await redis.get(cacheKeyLayer1);
+    console.timeEnd("Cache layer 2 Miss");
+    if (!cacheDataLayer1) {
+      // Check third party datasource
+      const data = await apiRequest3rdPartyDataSource(pokemonNo);
+      const lightweightData = spritesOnlyDefault(data.sprites);
+      if (useCache) {
+        // Store lightweight details in layer 2 cache
+        console.time("Cache pokeapi-data to layer 2");
+        await cacheRequest(cacheKeyLayer2, lightweightData);
+        console.timeEnd("Cache pokeapi-data to layer 2");
 
-    if (!cachedDataLayer2) {
-      const cacheKeyLayer1 = pokemonNo;
-      try {
-        console.time("Cache layer 2 Miss");
-        const cacheDataLayer1 = useCache
-          ? await redis.get(cacheKeyLayer1)
-          : null;
-        console.timeEnd("Cache layer 2 Miss");
-
-        if (!cacheDataLayer1) {
-          try {
-            const data = await getDataFrom3rdPartySource(pokemonNo);
-            return res.json(data);
-          } catch (error) {
-            return res.status(500).send("An error occurred");
-          }
-        }
-        console.time("Cache layer 1 Hit");
-        const resParsedFromCache = JSON.parse(cacheDataLayer1);
-        console.timeEnd("Cache layer 1 Hit");
-
-        const sprites = resParsedFromCache.sprites;
-        const filteredSprites = spritesOnlyDefault(sprites);
-
-        await redis.setex(
-          cacheKeyLayer2,
-          3600,
-          JSON.stringify(filteredSprites)
-        );
-
-        return res.json(filteredSprites);
-      } catch {
-        return res.status(500).send("An error occurred");
+        // Store details in layer 1 cache
+        console.time("Cache pokeapi-data to layer 1");
+        const cacheKeyLayer1 = pokemonNo;
+        await cacheRequest(cacheKeyLayer1, data);
+        console.timeEnd("Cache pokeapi-data to layer 1");
       }
+      return res.json(lightweightData);
     }
-    console.time("Cache layer 2 Hit");
-    const resParsedFromCache = JSON.parse(cachedDataLayer2);
-    console.timeEnd("Cache layer 2 Hit");
-
-    return res.json(resParsedFromCache);
-  } catch {
-    return res.status(500).send("An error occurred");
+    const lightweightData = spritesOnlyDefault(
+      JSON.parse(cacheDataLayer1).sprites
+    );
+    if (useCache) {
+      // Store lightweight details in layer 2 cache
+      console.time("Cache pokeapi-data to layer 2");
+      await cacheRequest(cacheKeyLayer2, lightweightData);
+      console.timeEnd("Cache pokeapi-data to layer 2");
+    }
+    return res.json(lightweightData);
   }
+  console.time("Cache hit layer 2");
+  const cachedRes = JSON.parse(cacheDataLayer2);
+  console.timeEnd("Cache hit layer 2");
+  return res.json(cachedRes);
 });
 
 module.exports = router;
--- routes/pokemon/pseudocode.md
+++ routes/pokemon/pseudocode.md
@@ -22,5 +22,22 @@ cache lw data from detailed cache
 
 
 # what logic can be shared between calls?
-caching
 
+## on detailed request
+no layer 1 cache
+- get from 3rd party
+
+with layer 1 cache
+- get from layer 1
+
+
+## on lw request
+no layer 2 cache
+- get from layer 1
+no layer 1 cache
+- get from 3rd party
+- store in layer 1
+- store in layer 2
+
+with layer 2 cache
+- get from layer 2
